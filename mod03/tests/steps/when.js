beb93149d9d00999d3ab2d8f655ab0304f9b6361
Jinn Koriech <jinn@ixydo.com>
2021-11-26 15:20:58 +0000

    16: Set up eventbus & sns into SQS for testing
    We use an SNS subscription to get the SNS messages into SQS,
    and we set up an EventBridge rule to send events of a specific
    structure to also go into SQS.

--- mod03/tests/steps/when.js
+++ mod03/tests/steps/when.js
@@ -4,8 +4,21 @@ const _ = require('lodash')
 const aws4 = require('aws4')
 const URL = require('url')
 const http = require('axios')
+const EventBridge = require('aws-sdk/clients/eventbridge')
 
 const mode = process.env.TEST_MODE
+
+const viaEventBridge = async (busName, source, detailType, detail) => {
+  const eventBridge = new EventBridge()
+  await eventBridge.putEvents({
+    Entries: [{
+      Source: source,
+      DetailType: detailType,
+      Detail: JSON.stringify(detail),
+      EventBusName: busName
+    }]
+  }).promise()
+}
 
 const viaHandler = async (event, functionName) => {
   const handler = require(`${APP_ROOT}/functions/${functionName}`).handler
@@ -124,10 +137,10 @@ const we_invoke_notify_restaurant = async (event) => {
   if (mode === 'handler') {
     await viaHandler(event, 'notify-restaurant')
   } else {
-    throw new Error('not supported')
+    const busName = process.env.bus_name
+    await viaEventBridge(busName, event.source, event['detail-type'], event.detail)
   }
 }
-
 
 module.exports = {
   we_invoke_get_index,
